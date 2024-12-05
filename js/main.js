0fb50b74f497c4922acc44af50f1f0e81200145e
yyyy7246 <yyyy7246@naver.com>
2024-12-05 14:46:49 +0900

    chore: 등수 문구수정

--- js/main.js
+++ js/main.js
@@ -539,8 +539,8 @@ async function submitResult() {
           <div class="ranking-info">
               <h3>등급 정보</h3>
               <p class="percentile">당신의 개인정보영역 등급은 ${getGrade(rankData.percentile)}등급입니다! (상위 ${rankData.percentile.toFixed(1)}%)</p>
-              <p class="rank-detail">총 ${rankData.actualParticipants}명 중 공동 ${rankData.currentRank}등 입니다.</p>
-              <p style="font-size: 0.9rem; color: #666; margin-top: -10px; word-break: keep-all; word-wrap: break-word;">💡 순위에 보이지 않는다면 하단에 새로고침 버튼을 눌러주세요</p>
+              <p class="rank-detail">전체 참여자 ${rankData.actualParticipants}명 중 공동 ${rankData.currentRank}등 입니다.</p>
+              <p style="font-size: 0.9rem; color: #666; margin-top: -10px; word-break: keep-all; word-wrap: break-word;">💡 순위에 보이지 않는다면 하단에 새로고침 버튼을 눌러주세요<br> ⏰ 하단의 순위는 오늘 참여자 기준으로 집계됩니다</p>
               <div class="top-rankers">
                   <h4>오늘의 상위 10명</h4>
                   <div class="ranking-list">
